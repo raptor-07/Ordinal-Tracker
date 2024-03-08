a3981d18ae1cbab0b94c4ed0d46d8fefb901b3b2
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-08 17:53:50 +0530

    style: fix bg

--- src/app/dashboard/components/TopBar.tsx
+++ src/app/dashboard/components/TopBar.tsx
@@ -13,7 +13,6 @@ import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import Paper from "@mui/material/Paper";
-import { ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import theme from "../../theme";
@@ -22,8 +21,8 @@ function TopBar() {
   const pages = ["Collections", "Watch List", "Alerts"];
 
   let initialWallets: any[] | (() => any[]) = [];
-  
-  if (typeof window !== 'undefined') {
+
+  if (typeof window !== "undefined") {
     const walletsString = localStorage.getItem("wallets");
     initialWallets = walletsString ? walletsString.split(",") : [];
   }
@@ -52,235 +51,253 @@ function TopBar() {
   const handleAddWallet = () => {
     const updatedWallets = [...wallets, newWallet];
     setWallets(updatedWallets);
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       localStorage.setItem("wallets", updatedWallets.join(","));
     }
     setNewWallet("");
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <AppBar
-        position="sticky"
-        color="primary"
+    <AppBar
+      enableColorOnDark
+      position="sticky"
+      color="primary"
+      style={{
+        backgroundColor: "#000000",
+        color: "white",
+        boxShadow: "none",
+        margin: 0,
+        padding: 0,
+      }}
+    >
+      <Container
+        maxWidth="xl"
+        sx={{
+          margin: "0",
+          minWidth: "100%",
+          padding: "0",
+        }}
         style={{
-          backgroundColor: theme.palette.background.default,
-          color: "white",
-          boxShadow: "none",
-          margin: 0,
-          padding: 0,
+          backgroundColor: "#000000",
         }}
       >
-        <Container
-          maxWidth="xl"
-          sx={{
-            margin: "0",
-            minWidth: "100%",
-            padding: "0",
-          }}
+        <Toolbar
+        style={{
+          backgroundColor: "#000000",
+        }}
         >
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 5,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              OrdiTrack
-            </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 5,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            OrdiTrack
+          </Typography>
 
-            <Box
-              sx={{
-                display: { xs: "flex", md: "flex" },
-                justifyContent: "space-evenly",
+          <Box
+            sx={{
+              display: { xs: "flex", md: "flex" },
+              justifyContent: "space-evenly",
+            }}
+            style={{
+              backgroundColor: "#000000",
+            }}
+          >
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
               }}
-            >
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "flex" },
-                  mt: "45px",
-                  justifyContent: "center",
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
               }}
-            >
-              OrdiTrack
-            </Typography>
-            <Box
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: "none", md: "flex" },
-                justifyContent: "space-around",
-                alignItems: "center",
-                minWidth: "80%",
-                m: "0 30px 0 0",
+                display: { xs: "block", md: "flex" },
+                mt: "45px",
+                justifyContent: "center",
               }}
             >
               {pages.map((page) => (
-                <Paper
-                  elevation={0}
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2,
-                    padding: "1%",
-                    flexGrow: 1,
-                    m: 0,
-                    color: "white",
-                    display: "block",
-                    backgroundColor: "transparent",
-                    cursor: "pointer",
-                    boxShadow: "none",
-                    position: "relative", // Added for positioning the glowing effect
-                    "&:hover": {
-                      borderBottom: "2px solid #6a67c9",
-                      "& p": {
-                        textShadow: "0 0 20px #6a67c9", // Increased intensity of text shadow
-                        color: "#C5C2F1",
-                        fontSize: "16.2px",
-                        fontWeight: "700", // Change text color on hover
-                      },
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+            style={{
+              backgroundColor: "#000000",
+            }}
+          >
+            OrdiTrack
+          </Typography>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex" },
+              justifyContent: "space-around",
+              alignItems: "center",
+              minWidth: "80%",
+              m: "0 30px 0 0",
+              maxHeight: "80%",
+            }}
+            style={{
+              backgroundColor: "#000000",
+            }}
+          >
+            {pages.map((page) => (
+              <Paper
+                elevation={0}
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  padding: "1.4%",
+                  flexGrow: 1,
+                  m: 0,
+                  color: "white",
+                  display: "block",
+                  backgroundColor: "transparent",
+                  cursor: "pointer",
+                  boxShadow: "none",
+                  position: "relative",
+                  "&:hover": {
+                    borderBottom: "1px solid #6a67c9",
+                    "& p": {
+                      textShadow: "0 0 5px #6a67c9",
+                      color: "#C5C2F1",
+                      fontWeight: "700",
                     },
-                    "&:active": {
-                      borderBottom: "2px solid #6a67c9", // Border bottom stays upon click
+                  },
+                  "&:active": {
+                    borderBottom: "2px solid #6a67c9",
+                  },
+                }}
+                style={{
+              backgroundColor: "#000000",
+            }}
+              >
+                <p
+                  style={{
+                    textAlign: "center",
+                    margin: 0,
+                    transition: "text-shadow 0.3s ease, color 0.3s ease",
+                  }}
+                >
+                  {page}
+                </p>
+              </Paper>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open wallets">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="User"
+                  src="/images/wallet.png"
+                  sx={{
+                    width: 70,
+                    height: 70,
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  p: 2,
+                }}
+              >
+                <TextField
+                  label="New Wallet"
+                  value={newWallet}
+                  onChange={(e) => setNewWallet(e.target.value)}
+                />
+                <Button
+                  onClick={handleAddWallet}
+                  sx={{
+                    mt: 2,
+                    backgroundColor: "transparent",
+                    boxShadow: "none",
+                    width: "100%",
+                    "&:hover": {
+                      boxShadow: "0px 0px 10px 0px rgba(106, 103, 201, 0.5)",
+                      textDecorationColor: "white",
                     },
                   }}
                 >
                   <p
                     style={{
-                      textAlign: "center",
-                      margin: 0,
-                      transition: "text-shadow 0.3s ease, color 0.3s ease", // Added transition for text shadow and color
+                      color: "#6a67c9",
+                      fontWeight: 700,
+                      padding: "0",
+                      margin: "0",
                     }}
                   >
-                    {page}
+                    ADD WALLET
                   </p>
-                </Paper>
+                </Button>
+              </Box>
+              {wallets.map((wallet_id) => (
+                <MenuItem key={wallet_id} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{wallet_id}</Typography>
+                </MenuItem>
               ))}
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open wallets">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar
-                    alt="User"
-                    src="/images/wallet.png"
-                    sx={{
-                      width: 70,
-                      height: 70,
-                    }}
-                  />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    p: 2,
-                  }}
-                >
-                  <TextField
-                    label="New Wallet"
-                    value={newWallet}
-                    onChange={(e) => setNewWallet(e.target.value)}
-                  />
-                  <Button
-                    onClick={handleAddWallet}
-                    sx={{
-                      mt: 2,
-                      backgroundColor: "transparent",
-                      boxShadow: "none",
-                      width: "100%",
-                      "&:hover": {
-                        boxShadow: "0px 0px 10px 0px rgba(106, 103, 201, 0.5)",
-                        textDecorationColor: "white",
-                      },
-                    }}
-                  >
-                    <p
-                      style={{
-                        color: "#6a67c9",
-                        fontWeight: 700,
-                        padding: "0",
-                        margin: "0",
-                      }}
-                    >
-                      ADD WALLET
-                    </p>
-                  </Button>
-                </Box>
-                {wallets.map((wallet_id) => (
-                  <MenuItem key={wallet_id} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{wallet_id}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </ThemeProvider>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 export default TopBar;
