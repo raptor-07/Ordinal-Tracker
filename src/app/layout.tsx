5f679e7adb52572b12ff3560600dc015d0f70ab1
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-11 19:30:04 +0530

    config: MUI - Next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme/theme";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
