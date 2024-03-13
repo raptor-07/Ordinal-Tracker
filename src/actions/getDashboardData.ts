795a5c56e946b115e55314259847d2dafad7993e
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-13 19:36:23 +0530

    feat: integrate pipeline 2

--- src/actions/getDashboardData.ts
+++ src/actions/getDashboardData.ts
@@ -9,42 +9,47 @@ async function getDashboardData(
   userEmail: string | null,
   wallets: string | null
 ) {
-  if (userEmail == null) {
-    // No session
-    return null;
-  } else {
-    console.log("userEmail inside get dashboard servaction", userEmail);
-    const user = await getUserByEmail(userEmail);
-    if (user == null) {
-      return { null: null };
+  try {
+    if (userEmail == null) {
+      // No session
+      return null;
+    } else {
+      console.log("userEmail inside get dashboard servaction", userEmail);
+      const user = await getUserByEmail(userEmail);
+      if (user == null) {
+        return { null: null };
+      }
+
+      // console.log("wallets inside get dashboard servaction", wallets);
+      const collectionIds: string[] = await getCollectionIds(wallets);
+
+      const [collectionsStats, collectionsFloor] = await Promise.all([
+        getCollectionsStats(collectionIds),
+        getCollectionsFloor(collectionIds),
+      ]);
+
+      // console.log("collectionsStats", collectionsStats);
+      // console.log("collectionsFloor", collectionsFloor);
+
+      const collectionsStatsMap = collectionsStats.reduce((map: any, collection: any) => {
+        map[collection.collection_id] = collection;
+        return map;
+      }, {});
+
+      const mergedData = collectionsFloor.map((floorPrice: any) => {
+        const collectionStats = collectionsStatsMap[floorPrice.collection_id];
+        return {
+          ...collectionStats,
+          ...floorPrice,
+        };
+      });
+
+      console.log("mergedData", mergedData);
+      return mergedData;
     }
-
-    console.log("wallets inside get dashboard servaction", wallets);
-    const collectionIds: string[] = await getCollectionIds(wallets);
-    
-    const [collectionsStats, collectionsFloor] = await Promise.all([
-      getCollectionsStats(collectionIds),
-      getCollectionsFloor(collectionIds),
-    ]);
-
-    console.log("collectionsStats", collectionsStats);
-    console.log("collectionsFloor", collectionsFloor);
-
-    const collectionsStatsMap = collectionsStats.reduce((map: any, collection: any) => {
-      map[collection.collection_id] = collection;
-      return map;
-    }, {});
-    
-    const mergedData = collectionsFloor.map(floorPrice => {
-      const collectionStats = collectionsStatsMap[floorPrice.collection_id];
-      return {
-        ...collectionStats,
-        ...floorPrice,
-      };
-    });
-    
-    console.log("mergedData", mergedData);
-    return mergedData;
+  } catch (error) {
+    console.error("Error in getDashboardData:", error);
+    throw error; // Rethrow the error for the caller to handle
   }
 }
 
