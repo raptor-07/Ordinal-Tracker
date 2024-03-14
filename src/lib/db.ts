b399d0ffc56fd6da1c4cdee6457d7d9010849971
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-14 15:22:45 +0530

    fix:deploy:

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import Prisma from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+const { PrismaClient } = Prisma;
+export const db = new PrismaClient();
