f7cf5d50a473edd8fd3699d217c3015c9aa8792c
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-15 15:47:58 +0530

    feat: Create logic for "s1 | w0" - refractor:fix:conditional logic

--- src/actions/getDashboardData.ts
+++ src/actions/getDashboardData.ts
@@ -7,18 +7,26 @@ import getCollectionsFloor from "./getCollectionsFloor";
 import {
   addCollectionsToCollection,
   addCollectionsToUserCollection,
+  getUserCollections,
 } from "@/data/collection";
+import { getUserWallets } from "@/data/wallet";
 
 async function getDashboardData(
   userEmail: string | null | undefined,
   wallets: string | null
 ) {
   try {
+    console.log("userEmail", userEmail);
+    console.log("wallets", wallets);
+
     if (
-      userEmail == null ||
-      userEmail === "" ||
-      (userEmail === undefined && wallets !== null)
+      (userEmail == null || userEmail === "" || userEmail === undefined) &&
+      wallets !== null &&
+      wallets !== "" &&
+      wallets !== undefined
     ) {
+      //Session 0 | Wallets 1
+      console.log("Session 0 | Wallets 1");
       const collectionIds: string[] = await getCollectionIds(wallets);
 
       const [collectionsStats, collectionsFloor] = await Promise.all([
@@ -43,9 +51,19 @@ async function getDashboardData(
       });
 
       return mergedData;
-    } else {
-      //with session
-
+    } else if (
+      userEmail !== null &&
+      userEmail !== "" &&
+      userEmail !== undefined &&
+      wallets !== null &&
+      wallets !== "" &&
+      wallets !== undefined
+    ) {
+      //Session 1 | Wallets 1
+      console.log("Session 1 | Wallets 1");
+      if (userEmail === null || userEmail === "" || userEmail === undefined) {
+        return { error: "user does not exist" };
+      }
       const user = await getUserByEmail(userEmail);
       if (user == null) {
         return { error: "user does not exist" };
@@ -84,6 +102,47 @@ async function getDashboardData(
 
       console.log("mergedData", mergedData);
       return mergedData;
+    } else {
+      //Session 1 | Wallets 0
+      console.log("Session 1 | Wallets 0");
+      if (userEmail === null || userEmail === "" || userEmail === undefined) {
+        return { error: "user does not exist" };
+      }
+      const user = await getUserByEmail(userEmail);
+      if (user == null) {
+        return { error: "user does not exist" };
+      }
+      const collectionIds: any = await getUserCollections(user);
+      const collectionIdStrings = collectionIds.map(
+        (item: any) => item.collectionId
+      );
+
+      const [collectionsStats, collectionsFloor] = await Promise.all([
+        getCollectionsStats(collectionIdStrings),
+        getCollectionsFloor(collectionIdStrings),
+      ]);
+
+      const collectionsStatsMap = collectionsStats.reduce(
+        (map: any, collection: any) => {
+          map[collection.collection_id] = collection;
+          return map;
+        },
+        {}
+      );
+
+      const mergedData = collectionsFloor.map((floorPrice: any) => {
+        const collectionStats = collectionsStatsMap[floorPrice.collection_id];
+        return {
+          ...collectionStats,
+          ...floorPrice,
+        };
+      });
+
+      const userWallets = await getUserWallets(user);
+      const userWalletsString = userWallets
+        .map((wallet: any) => wallet.wId)
+        .join(",");
+      return { wallets: userWalletsString, data: mergedData };
     }
   } catch (error) {
     console.error("Error in getDashboardData:", error);
