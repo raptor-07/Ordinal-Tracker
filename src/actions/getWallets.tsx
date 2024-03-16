0650d66602a8e4f012897d350368a1d5108d46e4
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-17 03:11:56 +0530

    feat: server action to fetch wallets

---
+++ src/actions/getWallets.tsx
@@ -0,0 +1,14 @@
+"use server";
+
+import { getUserByEmail } from "@/data/user";
+import { User, getUserWallets } from "@/data/wallet";
+
+export const getWallets = async (userRef: any) => {
+  console.log("userRef", userRef);
+  const user: User | any = await getUserByEmail(userRef.current.email);
+  if (!user) {
+    return null;
+  }
+  const wallets = await getUserWallets(user);
+  return wallets;
+};
