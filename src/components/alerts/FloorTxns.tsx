2529e3246517abc24c0b9b3194a837f9fef5396e
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-17 03:13:02 +0530

    scaffold: floor alerts component

---
+++ src/components/alerts/FloorTxns.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface FloorTxnsProps {}
+
+const FloorTxns: React.FC<FloorTxnsProps> = (props) => {
+  return <div>Watchlist alerts</div>;
+};
+
+export default FloorTxns;
