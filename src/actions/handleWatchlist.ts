e7122d4f4251005506a7bc7a774e0275657c3591
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-16 18:39:10 +0530

    feat: watchlist functionalities

--- src/actions/handleWatchlist.ts
+++ src/actions/handleWatchlist.ts
@@ -9,7 +9,12 @@ import {
 import { db } from "@/lib/db";
 
 interface data {
-  watchlists?: string[];
+  watchlists?: {
+    collectionId: string;
+    description: string;
+    image: string;
+    name: string;
+  }[];
   error?: string;
 }
 
@@ -20,9 +25,30 @@ export const getWatchlists = async (userRef: any): Promise<data> => {
         email: userRef.current.email,
       },
     });
-    //TODO: handle logout here, Remove session
-    const collections = await getWatchlistCollections(user);
-    return {};
+
+    if (!user) {
+      return { error: "Please login to view your watchlist" };
+    }
+
+    const collectionsInWatchlist = await getWatchlistCollections(user);
+
+    const watchlists: any = await Promise.all(
+      collectionsInWatchlist.map(async (collection) => {
+        const collectionDetails = await getCollectionById(
+          collection.collectionId
+        );
+        return {
+          name: collectionDetails?.name,
+          image: collectionDetails?.image,
+          collection_id: collection.collectionId,
+          description: collectionDetails?.description,
+        };
+      })
+    );
+    console.log("watchlists in server action", watchlists);
+    return {
+      watchlists,
+    };
   } catch (error: any) {
     console.error("Error in getWatchlists:", error);
     return { error: error };
@@ -39,6 +65,10 @@ export const addWatchlist = async (slug: string, userRef: any) => {
     });
     console.log("user from db", user);
     //TODO: handle logout here, Remove session
+    if (!user) {
+      return { error: "Please login to add a collection to your watchlist" };
+    }
+
     const formatSlug = (slug: string) => {
       let normalized = slug.toLowerCase();
 
@@ -46,8 +76,10 @@ export const addWatchlist = async (slug: string, userRef: any) => {
 
       return normalized;
     };
+    //format slug
     const formattedSlug: string = formatSlug(slug);
     console.log("formattedSlug", formattedSlug);
+
     //get inscription
     const inscription = await fetch(
       `https://turbo.ordinalswallet.com/collection/${formattedSlug}`,
@@ -62,8 +94,8 @@ export const addWatchlist = async (slug: string, userRef: any) => {
 
     const inscriptionNumber = inscriptionData.highest_inscription_num;
     console.log("inscription", inscriptionNumber);
-    //get collection id
 
+    //get collection id
     const headers = new Headers();
     headers.append("x-api-key", process.env.SIMPLE_HASH ?? "");
 
@@ -74,8 +106,10 @@ export const addWatchlist = async (slug: string, userRef: any) => {
         headers: headers,
       }
     );
+
     //verify response
     const response = await collectionIdFromSlug.json();
+
     // console.log("response", response);
     if (response.status !== 200) {
       if (response.status === 404) {
@@ -90,7 +124,7 @@ export const addWatchlist = async (slug: string, userRef: any) => {
     const collectionId = response.collection.collection_id;
     console.log("collectionId", collectionId);
     //Add to collections if does not exist
-    let collectionDetails;
+    let collectionDetails: any;
     if (collectionId.error) {
       collectionDetails = await addCollectionsToCollection(
         [collectionId],
@@ -98,13 +132,22 @@ export const addWatchlist = async (slug: string, userRef: any) => {
       );
     }
     collectionDetails = await getCollectionById(collectionId);
+    if (collectionDetails.error) {
+      return { error: collectionDetails.error };
+    }
     console.log("collectionDetails", collectionDetails);
 
     //check if it exists in user_watchlist, if not, create
     const inwatchlist = await isWatchlistCollection(collectionId, user);
     console.log("inwatchlist", inwatchlist);
+    
     //return collection details
-    return collectionDetails;
+    return ({
+      name: collectionDetails.name,
+      image: collectionDetails.image,
+      collection_id: collectionDetails.collectionId,
+      description: collectionDetails.description,
+    } = collectionDetails);
   } catch (error: any) {
     console.error("Error in addWatchlist:", error);
     return { error: error };
