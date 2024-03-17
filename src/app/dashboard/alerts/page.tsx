e5378939ddaafc933002aabfe634f5dd483c22d0
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-17 14:42:21 +0530

    feat: create reload state cycle

--- src/app/dashboard/alerts/page.tsx
+++ src/app/dashboard/alerts/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { Container } from "@mui/material";
 import React from "react";
 import FloorTxns from "@/components/alerts/FloorTxns";
 import WalletTxns from "@/components/alerts/WalletTxns";
+import ActiveAlerts from "@/components/alerts/ActiveAlerts";
+import { useCurrentUser } from "@/hooks/current-user";
+import { useRouter } from "next/navigation";
 
 interface Props {}
 
 const MyComponent: React.FC<Props> = (props) => {
+  const user = useCurrentUser();
+  const router = useRouter();
+
+  if (typeof window !== "undefined" && !user) {
+    alert("You are not logged in");
+    router.push("/auth/signin");
+    return null;
+  }
+
+  const [reload, setReload] = React.useState<Boolean>(false);
   return (
     <Container
       disableGutters
@@ -15,7 +30,8 @@ const MyComponent: React.FC<Props> = (props) => {
       }}
     >
       <WalletTxns />
-      <FloorTxns />
+      <FloorTxns setReload={setReload} />
+      <ActiveAlerts reload={reload} setReload={setReload} />
     </Container>
   );
 };
