5a6bdde8d0920f84300964ba6d1cd2b4ce8fa635
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-20 19:49:33 +0530

    feat: delete user collections associated with a wallet

--- src/actions/deleteWallet.ts
+++ src/actions/deleteWallet.ts
@@ -3,12 +3,18 @@
 import { deleteExistingWallet } from "@/data/wallet";
 import { getUserByEmail } from "@/data/user";
 import { User } from "next-auth";
+import { deleteUserCollectionsByWallet } from "@/data/collection";
 
 export const deleteWallet = async (user: User, walletString: string) => {
   try {
+    console.log("inside deleteWallet");
     const existingUser = await getUserByEmail(user.email ?? "");
     if (existingUser) {
-      return await deleteExistingWallet(walletString, existingUser);
+      console.log("deleteUserCollectionsByWallet");
+      await deleteUserCollectionsByWallet(walletString, existingUser.uId);
+      console.log("deleteExistingWallet");
+      await deleteExistingWallet(walletString, existingUser);
+      return {};
     }
     return { error: "User does not exist" };
   } catch (error: any) {
