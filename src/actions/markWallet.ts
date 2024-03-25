e3e4cfd13957f727d85b3335bac90531592026cd
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-25 08:22:47 +0530

    fix: refractor: mark wallet action

--- src/actions/markWallet.ts
+++ src/actions/markWallet.ts
@@ -1,7 +1,25 @@
 "use server";
-import { markWalletAs } from "@/data/wallet";
+import { getUserByEmail } from "@/data/user";
+import { getUserWallets, markWalletAs } from "@/data/wallet";
 
-export const markWallet = async (walletId: string, mark: boolean) => {
-  const result = await markWalletAs(walletId, mark);
-  return result;
+export const markWallet = async (
+  walletId: string,
+  mark: boolean,
+  userRef: any
+) => {
+  const user: any = await getUserByEmail(userRef.email);
+
+  if (!user) {
+    return { error: "User not found" };
+  }
+
+  const result = await markWalletAs(user.uId, walletId, mark);
+
+  if (result == null) {
+    return { error: "Wallet does not exist" };
+  }
+
+  const newWallets = await getUserWallets(user);
+
+  return newWallets;
 };
