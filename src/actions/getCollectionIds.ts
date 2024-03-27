aaf7eda3aedb3b488dead5682b1edf578412b18c
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-27 16:42:56 +0530

    feat: refractor: api endpoint

--- src/actions/getCollectionIds.ts
+++ src/actions/getCollectionIds.ts
@@ -1,17 +1,32 @@
-"use server";
-
 import dotenv from "dotenv";
 dotenv.config();
 
-async function getCollectionIds(wallets: string | null) {
+interface CollectionDetails {
+  collection_id: string;
+  name: string;
+  description: string;
+  image_url: string;
+  distinct_owner_count: number;
+  distinct_nft_count: number;
+  total_quantity: number;
+}
+
+async function getCollectionIds(wallets: string | null): Promise<
+  | {
+      collectionIds: Record<string, string[]>;
+      collectionDetails: CollectionDetails[];
+    }
+  | undefined
+> {
   let apikey: string = process.env.SIMPLE_HASH ?? "";
+  let collectionDetails: CollectionDetails[] = [];
 
   const fetchData = async (wallet: string) => {
     const headers = new Headers();
     headers.append("x-api-key", apikey);
 
     const response = await fetch(
-      `https://api.simplehash.com/api/v0/nfts/transfers/wallets?chains=bitcoin&wallet_addresses=${wallet}`,
+      `https://api.simplehash.com/api/v0/nfts/collections_by_wallets_v2?chains=bitcoin&wallet_addresses=${wallet}`,
       {
         method: "GET",
         headers: headers,
@@ -21,14 +36,23 @@ async function getCollectionIds(wallets: string | null) {
     if (response.status === 200) {
       const jsonData = await response.json();
 
-      const filteredTransfers = jsonData.transfers.filter(
-        (transfer: any) =>
-          transfer.chain === "bitcoin" && transfer.collection_id !== null
-      );
+      const ids = jsonData.collections
+        .map((collection: any) => collection.collection_id)
+        .filter((id: string | null) => id !== null);
 
-      const ids = filteredTransfers.map(
-        (transfer: any) => transfer.collection_id
-      );
+      collectionDetails = [
+        ...collectionDetails,
+        ...jsonData.collections.map((collection: any) => ({
+          collection_id: collection.collection_id,
+          name: collection.collection_details.name,
+          description: collection.collection_details.description,
+          image_url: collection.collection_details.image_url,
+          distinct_owner_count:
+            collection.collection_details.distinct_owner_count,
+          distinct_nft_count: collection.collection_details.distinct_nft_count,
+          total_quantity: collection.collection_details.total_quantity,
+        })),
+      ];
 
       return { [wallet]: ids };
     } else {
@@ -45,7 +69,21 @@ async function getCollectionIds(wallets: string | null) {
 
       const result = Object.assign({}, ...walletCollections);
       console.log("result", result);
-      return result;
+
+      // Remove duplicates and null values from collectionDetails
+      collectionDetails = collectionDetails.filter(
+        (collection, index, self) =>
+          collection.collection_id !== null &&
+          index ===
+            self.findIndex((t) => t.collection_id === collection.collection_id)
+      );
+
+      console.log("collectionDetails", {
+        collectionIds: result,
+        collectionDetails: collectionDetails,
+      });
+
+      return { collectionIds: result, collectionDetails: collectionDetails };
     }
   } catch (error) {
     console.error("Error fetching collection IDs:", error);
