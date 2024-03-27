4e489ab2f0429b48044e3eed02e07f225056a4a7
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-28 00:34:50 +0530

    feat: modify table header

--- src/components/watchlist/Table.tsx
+++ src/components/watchlist/Table.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,11 +8,24 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import {
+  Box,
+  CircularProgress,
+  Button,
+  Avatar,
+  Modal,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import { Star, StarBorder } from "@mui/icons-material";
 import { getWatchlists } from "@/actions/handleWatchlist";
 import { useCurrentUser } from "@/hooks/current-user";
 import { useRouter } from "next/navigation";
-import Typography from "@mui/material/Typography";
-import { Box } from "@mui/material";
+import { motion } from "framer-motion";
 
 export interface Watchlist {
   name: string;
@@ -34,6 +46,31 @@ export default function CollectionTable({
   const user: any = useCurrentUser();
   let userRef: any = React.useRef(user);
 
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      // rotate: 360,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        damping: 20,
+        duration: 1,
+      },
+    },
+    tap: { scale: 1.2 },
+  };
+
+  const starVariants = {
+    hover: {
+      scale: [1, 1.2, 1, 1.2, 1], // scales up and down
+      transition: {
+        duration: 2,
+        repeat: Infinity, // repeats the animation indefinitely
+      },
+    },
+    tap: { scale: 0.95 },
+  };
+
   React.useEffect(() => {
     //get watchlist data from db - initial fetch
     const fetchData = async () => {
@@ -52,6 +89,21 @@ export default function CollectionTable({
     };
     fetchData();
   }, []);
+
+  const handleRefetch = async () => {
+    // const data = await getWatchlists(userRef);
+    // if (data.error) {
+    //   if (data.error === "Please login to view your watchlist") {
+    //     alert(data.error);
+    //     router.push("/auth/signin");
+    //   }
+    //   alert(data.error);
+    //   return;
+    // }
+    // if (data.watchlists) {
+    //   setWatchlist(data.watchlists);
+    // }
+  };
 
   return (
     <TableContainer
@@ -82,22 +134,56 @@ export default function CollectionTable({
               padding: "0",
             }}
           >
-            <TableCell
-              sx={{
-                flexGrow: 1,
-              }}
-            >
-              <p
-                style={{
-                  fontWeight: 700,
-                  margin: "0 0 0 8px",
-                  textDecorationLine: "underline",
-                  textUnderlineOffset: "4px",
-                  padding: "0",
+            <TableCell>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                  // width: "min-content",
+                  // maxWidth: "50px",
                 }}
               >
-                Name
-              </p>
+                <Button
+                  onClick={handleRefetch}
+                  sx={{
+                    backgroundColor: "#000000",
+                    color: "#ffffff",
+                    padding: "0",
+                    margin: "0",
+                    paddingTop: "4px",
+                    "&:hover": {
+                      backgroundColor: "#000000",
+                      color: "#ffffff",
+                    },
+                  }}
+                >
+                  <motion.div
+                    whileHover="hover"
+                    whileTap="tap"
+                    variants={buttonVariants}
+                  >
+                    <RefreshIcon
+                      sx={{
+                        marginLeft: "-24px",
+                        marginTop: "10px",
+                      }}
+                    />
+                  </motion.div>
+                </Button>
+                <p
+                  style={{
+                    fontWeight: 700,
+                    marginLeft: "8px",
+                    textDecorationLine: "underline",
+                    textUnderlineOffset: "4px",
+                    padding: "0",
+                    margin: "0",
+                  }}
+                >
+                  Name
+                </p>
+              </Box>
             </TableCell>
             <TableCell
               align="left"
