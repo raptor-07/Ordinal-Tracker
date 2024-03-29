8d40c212a47d2347973b4976337a6a07701bfbd6
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-29 16:43:44 +0530

    refractor: fix: change api endpoint

--- src/actions/getDashboardData.ts
+++ src/actions/getDashboardData.ts
@@ -7,6 +7,7 @@ import getCollectionsFloor from "./getCollectionsFloor";
 import {
   addCollectionsToCollection,
   addCollectionsToUserCollection,
+  getCollectionById,
   getUserCollections,
 } from "@/data/collection";
 import { getUserWallets } from "@/data/wallet";
@@ -205,6 +206,21 @@ async function getDashboardData(
         return { error: collectionIds.error };
       }
 
+      //get collection details for every id
+      const collectionDetails: any = await Promise.all(
+        collectionIds.map(async (collection: any) => {
+          const result: any = await getCollectionById(collection);
+          return {
+            collection, image_url: result.image, distinct_owner_count: result.owner_count, distinct_nft_count: result.nft_count,
+            total_quantity: result.nft_count
+          };
+        })
+      );
+      const collectionDetailsMap = collectionDetails.reduce((map: any, collection: any) => {
+        map[collection.collection] = { image_url: collection.image_url, distinct_owner_count: collection.distinct_owner_count, distinct_nft_count: collection.distinct_nft_count, total_quantity: collection.total_quantity };
+        return map;
+      }, {})
+
       // console.log("collectionIds", collectionIds);
       const [collectionsStats, collectionsFloor] = await Promise.all([
         getCollectionsStats(collectionIds),
@@ -226,9 +242,11 @@ async function getDashboardData(
 
       const mergedData = collectionsFloor.map((floorPrice: any) => {
         const collectionStats = collectionsStatsMap[floorPrice.collection_id];
+        const collectionDetail = collectionDetailsMap[floorPrice.collection_id];
         return {
           ...collectionStats,
           ...floorPrice,
+          ...collectionDetail,
         };
       });
 
