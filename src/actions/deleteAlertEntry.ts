223cf37dea8a831c4abd0b18320f721e1e7c8877
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-04 06:09:02 +0530

    feat: create action to delete alert entry attached to a user and a collection

--- src/actions/deleteAlertEntry.ts
+++ src/actions/deleteAlertEntry.ts
@@ -3,6 +3,7 @@
 import { getUserByEmail } from "@/data/user";
 import {
   deleteAlertEnryById,
+  deleteAlertEntryByCollectionId,
 } from "@/data/collection";
 
 export const deleteAlertEntry = async (userRef: any, alertId: any) => {
@@ -25,3 +26,24 @@ export const deleteAlertEntry = async (userRef: any, alertId: any) => {
     throw error;
   }
 };
+
+export const deleteAlertEntryByCollection = async (userRef: any, collectionId: any) => {
+  const user: any = await getUserByEmail(userRef.current.email);
+
+  if (!user) {
+    return {
+      error: "User not found",
+    };
+  }
+
+  try {
+    const alertEntry = await deleteAlertEntryByCollectionId(user, collectionId);
+    return {
+      success: true,
+      data: alertEntry,
+    };
+  } catch (error) {
+    console.error("Error in createAlertEntry:", error);
+    throw error;
+  }
+}
