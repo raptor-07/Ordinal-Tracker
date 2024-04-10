4a5664f03c67bf900c22e228e8f9c504fb3a5378
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-10 19:42:16 +0530

    feat: create db actions for token tables

--- src/data/user.ts
+++ src/data/user.ts
@@ -78,3 +78,27 @@ export const getVerificationTokenByEmail = async (
     return null;
   }
 }
+
+export const getPasswordResetTokenByEmail = async (email: string) => {
+  try {
+    const passwordResetToken = await db.passwordResetToken.findFirst({
+      where: { email }
+    });
+
+    return passwordResetToken;
+  } catch {
+    return null;
+  }
+};
+
+export const getPasswordResetTokenByToken = async (token: string) => {
+  try {
+    const passwordResetToken = await db.passwordResetToken.findUnique({
+      where: { token }
+    });
+
+    return passwordResetToken;
+  } catch {
+    return null;
+  }
+};
