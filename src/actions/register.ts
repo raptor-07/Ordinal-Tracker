df7e873be44a9285514ad5c54cbe1b5ec8ceb1f0
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-10 16:32:10 +0530

    feat: create token and send email upon registering

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -3,6 +3,9 @@ import { getUserByEmail } from "@/data/user";
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcryptjs";
+import { sendEmailVerification } from "@/lib/email";
+import { generateVerificationToken } from "@/lib/tokens";
+import { VerificationToken } from "@prisma/client";
 
 const register = async (values: {
   email: string;
@@ -16,12 +19,15 @@ const register = async (values: {
       return { error: "Validation error" };
     }
     const { email, password, name } = validateFields.data;
+    console.log("Registering user", email, password, name);
     const hashedPassword = await bcrypt.hash(password, 10);
     const existingUser = await getUserByEmail(email);
 
     if (existingUser) {
       return { error: "User already exists" };
     }
+
+    console.log("Creating user");
 
     const user = await db.user.create({
       data: {
@@ -30,6 +36,13 @@ const register = async (values: {
         username: name,
       },
     });
+    console.log("User created", user);
+    // genereate token
+    const token: VerificationToken = await generateVerificationToken(email);
+    console.log("Token generated", token);
+
+    //send verification email
+    await sendEmailVerification(token.email, token.token);
 
     return { user };
   } catch (error: any) {
