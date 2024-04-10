da44cc0b6fabf78c669bd1797f3f964b52318d69
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-10 19:43:13 +0530

    feat: create util to generate reset password token

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { db } from "@/lib/db";
-import { getVerificationTokenByEmail } from "@/data/user";
+import { getVerificationTokenByEmail, getPasswordResetTokenByEmail } from "@/data/user";
 
 
 export const generateVerificationToken = async (email: string) => {
@@ -27,3 +27,26 @@ export const generateVerificationToken = async (email: string) => {
 
     return verficationToken;
 };
+
+export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getPasswordResetTokenByEmail(email);
+
+    if (existingToken) {
+        await db.passwordResetToken.delete({
+            where: { id: existingToken.id }
+        });
+    }
+
+    const passwordResetToken = await db.passwordResetToken.create({
+        data: {
+            email,
+            token,
+            expires
+        }
+    });
+
+    return passwordResetToken;
+}
