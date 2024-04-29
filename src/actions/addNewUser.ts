abec9f4df7e0d167ebaaf57754109feb154c20f6
Hurmaan Ahmed @ mac <contactme.hurmaan@gmail.com>
2024-04-29 19:48:04 +0530

    feat: create action to add new User

--- src/actions/addNewUser.ts
+++ src/actions/addNewUser.ts
@@ -1,4 +1,41 @@
 "use server";
-import { z } from "zod";
 
-export const addNewUser = async () => {}
+import { TSignUpSchema } from "@/lib/types";
+import "@/envConfig";
+
+export const addNewUser = async (data: TSignUpSchema) => {
+  console.log(data);
+
+  const url = process.env.BACKEND_URL;
+  console.log(url);
+
+  try {
+    const result = await fetch(`${url}/auth/signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("res from nest", result);
+
+    if (result.ok) {
+      return {
+        success: true,
+      };
+    } else {
+      return {
+        success: false,
+      };
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    return {
+      success: false,
+    };
+  }
+};
