32177d81ae69786dfb4f1626136174c75fd35170
Hurmaan Ahmed @ mac <contactme.hurmaan@gmail.com>
2024-05-01 16:53:30 +0530

    feat: signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,40 @@
 "use client";
 
 import React, { useState } from "react";
-import { IconUserCircle } from '@tabler/icons-react';
+import { IconUserCircle } from "@tabler/icons-react";
+import { useCookies } from "react-cookie";
 
 export default function Home() {
+  const [cookies, setCookie, removeCookie] = useCookies(["jwt-token"]);
+
   const tabs = ["Portfolio", "Watchlist", "Alerts"];
   const [activeTab, setActiveTab] = useState("Portfolio");
+
+  const handleSignout = () => {
+    removeCookie("jwt-token");
+    return;
+  };
+
   return (
     <div className="container  mt-3 flex justify-between items-center">
       <p className="text-2xl">OrdiTrack</p>
       <div className="">
-      {tabs.map((tab) => {
-        return (
-          <button
-            key={tab}
-            onClick={() => setActiveTab(tab)}
-            className={`${
-              activeTab === tab ? " text-white" : " text-zinc-400"
-            } py-2 px-4 rounded`}
-          >
-            {tab}
-          </button>
-        );
-      })}
+        {tabs.map((tab) => {
+          return (
+            <button
+              key={tab}
+              onClick={() => setActiveTab(tab)}
+              className={`${
+                activeTab === tab ? " text-white" : " text-zinc-400"
+              } py-2 px-4 rounded`}
+            >
+              {tab}
+            </button>
+          );
+        })}
       </div>
-      <div className="hover:cursor-pointer">
-       <IconUserCircle size={32} />
+      <div className="hover:cursor-pointer" onClick={handleSignout}>
+        <IconUserCircle size={32} />
       </div>
     </div>
   );
