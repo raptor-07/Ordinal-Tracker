71c57b91b7c4ed8b22d46cd8b7aca49330f9a561
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-05-09 06:03:15 +0530

    fix: redirect test on local

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -62,7 +62,7 @@ const Page = () => {
     form.reset();
 
     try {
-      window.location.href = "http://192.168.0.121:3000/auth/google-signin";
+      window.location.href = "http://localhost:3000/auth/google-signin";
     } catch (error) {
       console.error("Error:", error);
     }
