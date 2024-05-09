c4b6d074894630141b4f7a3ee8a2bda11a2796ba
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-05-09 11:16:21 +0530

    config: for deploy

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -62,7 +62,7 @@ const Page = () => {
     form.reset();
 
     try {
-      window.location.href = "http://localhost:3000/auth/google-signin";
+      window.location.href = "https://ordinal-tracker-nest-be-7be2.onrender.com/auth/google-redirect/auth/google-signin";
     } catch (error) {
       console.error("Error:", error);
     }
