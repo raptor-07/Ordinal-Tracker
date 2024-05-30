f01de56396b6ddddda6433500c9c1773efdd3b16
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-05-30 22:08:29 +0530

    feat: create form schemas

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,18 +1,33 @@
-"use client"
+"use client";
 
-import { z } from "zod"
+import { z } from "zod";
 
 export const signupSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-    confirmPassword: z.string().min(8),
-})
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
 
 export type TSignUpSchema = z.infer<typeof signupSchema>;
 
 export const signinSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-})
+  email: z.string().email(),
+  password: z.string().min(8),
+});
 
 export type TSignInSchema = z.infer<typeof signinSchema>;
+
+export const sendResetTokenFormSchema = z.object({
+  email: z.string().email(),
+});
+
+export type TSendResetTokenFormSchema = z.infer<
+  typeof sendResetTokenFormSchema
+>;
+
+export const resetPasswordSchema = z.object({
+  newPassword: z.string().min(8),
+  confirmNewPassword: z.string().min(8),
+});
+
+export type TResetPasswordSchema = z.infer<typeof resetPasswordSchema>;
